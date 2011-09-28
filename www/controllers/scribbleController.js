4e3d8dead9297d978c3871ed9bc5ffd2c24ac7fc
Greg Allen <me@jga.me>
2011-09-28 07:58:21 -0700

    fixed view all logic

--- www/controllers/scribbleController.js
+++ www/controllers/scribbleController.js
@@ -68,7 +68,7 @@ var ScribbleController = Fidel.ViewController.extend({
     } else {
       this.newButton.css("visibility", "hidden");
     }
-    if (this.currentIndex > 0) {
+    if (this.scribbles.length !== 0) {
       this.viewAllButton.css('visibility', 'visible');
     } else {
       this.viewAllButton.css('visibility', 'hidden');
